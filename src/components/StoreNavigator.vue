5b87f0ae03b3c6c529160b74bbe1313652b6108d
rensothearin <ren.sothearin@gmail.com>
2025-10-20 22:37:19 +0900

    Connected the signup link to navigator

--- src/components/StoreNavigator.vue
+++ src/components/StoreNavigator.vue
@@ -149,7 +149,7 @@
               <div class="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
                 <a href="/signin" class="text-sm font-medium text-gray-700 hover:text-gray-800">Sign in</a>
                 <span class="h-6 w-px bg-gray-200" aria-hidden="true" />
-                <a href="#" class="text-sm font-medium text-gray-700 hover:text-gray-800">Create account</a>
+                <a href="/signup" class="text-sm font-medium text-gray-700 hover:text-gray-800">Create account</a>
               </div>
 
               <div class="hidden lg:ml-8 lg:flex">
