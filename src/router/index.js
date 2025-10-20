f33ab1569d501038ffa386577c6c5921129e1c18
rensothearin <ren.sothearin@gmail.com>
2025-10-20 22:36:00 +0900

    Added singin and singup page

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,20 @@
 // src/router/index.js
 
 import { createRouter, createWebHistory } from 'vue-router';
-import ProductForMenOverviewsPage from '../components/ProductForMenOverviewsPage.vue';
-import ProductForWomenOverviewsPage from '../components/ProductForWomenOverviewsPage.vue';
-import ProductCheckoutPage from '../components/ProductCheckoutPage.vue';
-import ShopStorePage from '../components/ShopStorePage.vue';
+import ProductForMenOverviewsPage from '../views/ProductForMenOverviewsPage.vue';
+import ProductForWomenOverviewsPage from '../views/ProductForWomenOverviewsPage.vue';
+import ProductCheckoutPage from '../views/ProductCheckoutPage.vue';
+import ShopStorePage from '../views/ShopStorePage.vue';
+import Signin from '../views/Signin.vue';
+import Signup from '../views/Signup.vue';
 
 const routes = [
   { path: '/women', component: ProductForWomenOverviewsPage },
   { path: '/men', component: ProductForMenOverviewsPage },
   { path: '/checkout', component: ProductCheckoutPage },
   { path: '/store-details', component: ShopStorePage },
+  { path: '/signin', component: Signin },
+  { path: '/signup', component: Signup },
 ];
 
 const router = createRouter({
